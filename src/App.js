49a44b17af92e2212350d44d491555e8457ba445
Pranshu-15 <pranshu923@gmail.com>
2023-11-25 21:38:37 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import './style.css'
-import React,{useState} from 'react';
-import Books from './Components/Books';
-import BookInfo from './Components/BookInfo';
+import SearchBar from './Components/SearchBar';
+import "./style.css"
 function App() {
-  const[bookinfo,setBookInfo]=useState([])
   return (
-    <div>
-       <BookInfo bookinfo={bookinfo}/>
-      <Books setBookInfo={setBookInfo}  bookinfo={bookinfo}/>
-     
+    <div className="App">
+        <SearchBar />
     </div>
   );
 }
